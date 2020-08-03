19b1b3b7bd345f01f40d6194e609fff31a9acdbe
mik3 <42175526+mik3sw@users.noreply.github.com>
2020-08-03 15:40:34 +0200

    Added check_permission command

--- bot/commands/__init__.py
+++ bot/commands/__init__.py
@@ -1,4 +1,4 @@
 __all__ = ["mute", "unmute", "help", "start", "rules", "ban",
-"io", "source", "pin", "say", "annuncio"]
+"io", "source", "pin", "say", "annuncio", "check"]
 
 from commands import *
