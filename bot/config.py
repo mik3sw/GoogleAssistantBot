f33eff675de35b5eb77e06cfce7da75e16ab649d
mik3 <42175526+mik3sw@users.noreply.github.com>
2020-08-03 15:42:19 +0200

    added TITLES and bot_id

--- bot/config.py
+++ bot/config.py
@@ -3,10 +3,11 @@
 
 bot_token = "000000000:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
 bot_username = "mybotusername"
-bot_id = 0123456789
+bot_id = 00000000000
 language = 'it' #check 'strings.ini' file [en, es, ru, ...]
 OWNER_LIST= [000000000, 11111111]
 LIST_OF_ADMINS = [11111111, 2222222, 3333333]
 COLLABORATORS = [44444444]
+TITLES = ['creator', 'administrator'] #do not change
 
 # =======================
