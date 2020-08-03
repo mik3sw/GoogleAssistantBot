4fc8a63f4ddf2d6a29a81d6351e2724e5c00b4a2
mik3 <42175526+mik3sw@users.noreply.github.com>
2020-08-03 15:41:29 +0200

    Added bot_id

--- bot/config.py
+++ bot/config.py
@@ -3,6 +3,7 @@
 
 bot_token = "000000000:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
 bot_username = "mybotusername"
+bot_id = 0123456789
 language = 'it' #check 'strings.ini' file [en, es, ru, ...]
 OWNER_LIST= [000000000, 11111111]
 LIST_OF_ADMINS = [11111111, 2222222, 3333333]
