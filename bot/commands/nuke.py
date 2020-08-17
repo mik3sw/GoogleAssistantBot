a1664531a7b2dd4a7f169316cb33d101d025d65c
mik3sw <mikedev.xda@gmail.com>
2020-08-17 12:58:59 +0200

    test

--- bot/commands/nuke.py
+++ bot/commands/nuke.py
@@ -25,22 +25,19 @@ def launch(update, context):
     query = update.callback_query
     query.answer()
     if query.data == 'nuke':
-        if query.from_user.id not in config.LIST_OF_ADMINS:
-            print('non admin')
-        else:
-            try:
-                n = 5
-                while n>0:
-                    query.edit_message_caption(caption="<b>LANCIO AUTORIZZATO</b>\n\nDetonazione in <b>{}</b>".format(n), parse_mode='HTML')
-                    time.sleep(1)
-                    n = n-1
-                context.bot.delete_message(update.callback_query.message.chat_id, update.callback_query.message.message_id)
-                print("done")
-                #https://i.pinimg.com/originals/6c/48/5e/6c485efad8b910e5289fc7968ea1d22f.gif
-                context.bot.send_video(update.callback_query.message.chat_id, 
-                    video='https://i.pinimg.com/originals/6c/48/5e/6c485efad8b910e5289fc7968ea1d22f.gif', 
-                    caption='<b>UTENTE NUCLEARIZZATO CON SUCCESSO</b>', parse_mode='HTML')
-            except:
-                print('Error')
+        try:
+            n = 5
+            while n>0:
+                query.edit_message_caption(caption="<b>LANCIO AUTORIZZATO</b>\n\nDetonazione in <b>{}</b>".format(n), parse_mode='HTML')
+                time.sleep(1)
+                n = n-1
+            context.bot.delete_message(update.callback_query.message.chat_id, update.callback_query.message.message_id)
+            print("done")
+            #https://i.pinimg.com/originals/6c/48/5e/6c485efad8b910e5289fc7968ea1d22f.gif
+            context.bot.send_video(update.callback_query.message.chat_id, 
+                video='https://i.pinimg.com/originals/6c/48/5e/6c485efad8b910e5289fc7968ea1d22f.gif', 
+                caption='<b>UTENTE NUCLEARIZZATO CON SUCCESSO</b>', parse_mode='HTML')
+        except:
+            print('Error')
                 
 
