81454c064f91336130bd722fc41e35033c006cf5
mik3sw <mikedev.xda@gmail.com>
2020-08-17 14:00:00 +0200

    test

--- bot/commands/nuke.py
+++ bot/commands/nuke.py
@@ -42,7 +42,7 @@ def launch(update, context):
             context.bot.send_video(update.callback_query.message.chat_id, 
                 video='https://i.pinimg.com/originals/6c/48/5e/6c485efad8b910e5289fc7968ea1d22f.gif', 
                 caption='<b>UTENTE NUCLEARIZZATO CON SUCCESSO</b>', parse_mode='HTML')
-            #context.bot.kick_chat_member(update.message.chat.id, stronzo, timeout=None, until_date=None)
+            #context.bot.kick_chat_member(update.message.chat.id, nuked, timeout=None, until_date=None)
             #print(stronzo)
 
         except:
@@ -51,4 +51,5 @@ def launch(update, context):
 
 def set_nuke(stronzo):
     nuked = stronzo
+    print(nuked)
 
