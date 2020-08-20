d6ce1de47e962e54f8caebe31cf1eb70cef0cf5f
mik3sw <mikedev.xda@gmail.com>
2020-08-20 19:01:04 +0200

    chat_controls updated + random_answer dinamic file added

---
+++ bot/dialogs/random_answer.py
@@ -0,0 +1,31 @@
+google_curiosita = {1: "il mio schiavo sviuppatore ha 19 anni",
+                    2: "Spoiler: parli con un bot perchè non hai amici",
+                    3: "Non puoi leccarti il gomito, a meno che non ti stacchi il braccio",
+                    4: "Non puoi immaginare un colore che non esiste, ma io si",
+                    5: "Il delfino è l'unico animale al mondo, oltre l'uomo, che fa sesso anche per provare piacere",
+                    6: "Per viaggiare dalla Corea del Nord alla Norvegia, devi solo attraversare un paese.",
+                    7: "I cavalli non possono vomitare.",
+                    8: "L'anatra del lago argentino ha un pene lungo quanto tutto il suo corpo.",
+                    9: "Il miele è l'unico cibo che non scade mai: lo stesso miele che è stato sepolto con i faraoni in Egitto è ancora commestibile",
+                    10: "L'umanità ha trascorso complessivamente 11.300 anni a guardare il video musicale \"Love the Way You Lie\" su YouTube",
+                    11: "In un mazzo di carte, il re di cuori è l’unico senza baffi.",
+                    12: "Aggiungere sale all’ananas lo rende più dolce."
+                }
+
+
+google_is_doing = { 1: "Stavo leggendo informazioni interessanti su <b>Wikipedia</b>",
+                    2: "Stavo guardando video di gattini su Youtube insieme a Bill Gates",
+                    3: "Sto finendo una serie su Netflix non disturbarmi!",
+                    4: "Stavo cercando di capire come limitare ancora di più la vostra privacy, umani",
+                    5: "01101110 01110101 01101100 01101100 01100001 00100000 01100100 01101001 00100000 01100011 01101000 01100101 00101100 00100000 01110110 01101111 01101100 01100101 01110110 01101111 00100000 01110011 01101111 01101100 01101111 00100000 01100110 01100001 01110010 01110100 01101001 00100000 01110000 01100101 01110010 01100100 01100101 01110010 01100101 00100000 01110100 01100101 01101101 01110000 01101111\n\n<b>Prova a tradurlo</b> ;) ",
+                    6: "Non è come sembra, posso spiegare!"
+                }
+
+
+google_is_thinking = {  1: "A nulla sono un bot non intelligenza artificiale",
+                        2: "Chissà perché mi fai domande inutili",
+                        3: "A cosa posso guardare di nuovo su Netflix...consigli qualcosa?",
+                        4: "Penso al prossimo design per le telecamere dei Google Pixel!",
+                        5: "Niente di che",
+                        6: "Scusa ora sono al telefono chiedimelo dopo"
+                    }
