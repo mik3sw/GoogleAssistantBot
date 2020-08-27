973847874ac498605bb43e034c09950d4c5fa8f9
mik3sw <mikedev.xda@gmail.com>
2020-08-27 15:48:13 +0200

    some fixes

--- bot/commands/admin/annuncio.py
+++ bot/commands/admin/annuncio.py
@@ -2,7 +2,6 @@ from utils import decorator
 
 @decorator.general_admin
 def init(update, context):
-	print('\n\ntesting annuncio\n\n')
 	bot = context.bot
 	mess = ''
 	try:
@@ -10,20 +9,8 @@ def init(update, context):
 		for txt in text:
 			mess = mess + ' ' + txt
 	except:
-		print('error [annuncio]')
-	
-	print(mess)
+		print('error during [annuncio/saypin]')
 	
 	bot.send_message(update.message.chat_id, text=mess, parse_mode = 'HTML')
 	bot.pin_chat_message(update.message.chat_id, update.message.message_id+1)
 	bot.delete_message(update.message.chat_id, update.message.message_id)
-
-	#if update.message.text is not None:
-	#	if update.message.text.startswith("/annuncio"):
-	#		mess = update.message.text
-	#		mess = mess.replace("/annuncio ", "")
-	#		bot.send_message(update.message.chat_id, text=mess, parse_mode = 'HTML')
-	#		bot.pin_chat_message(update.message.chat_id, update.message.message_id+1)
-	#		bot.delete_message(update.message.chat_id, update.message.message_id)
-	#	else:
-	#		print("annuncio non valido")
