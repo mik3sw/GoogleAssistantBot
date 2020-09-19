224c5b51608afe19c61dc04d39ca0154f028e3a5
mik3 <42175526+mik3sw@users.noreply.github.com>
2020-09-19 13:52:14 +0200

    useless send_video removed

--- bot/commands/admin/ban.py
+++ bot/commands/admin/ban.py
@@ -5,9 +5,6 @@ def init(update, context):
     pass
     bot = context.bot
     try:
-        bot.send_video(update.message.chat_id,
-        video='http://4.bp.blogspot.com/-HUn5hfk8OzQ/UM_Pi-bGphI/AAAAAAAAEVY/JO-DljB1L2I/s1600/explosi%25C3%25B3n+gif.gif',
-        caption='<b>QUESTA NON E\' UN\'ESERCITAZIONE</b>\n\nRecarsi immediatamente al bunker antiatomico, ripeto <b>NON E\' UN\'ESERCITAZIONE</b>', parse_mode='HTML')
         bot.kick_chat_member(update.message.chat.id, update.message.reply_to_message.from_user.id, timeout=None, until_date=None)
         update.message.reply_text("User: [{}][{}][{}]\nSuccesfully banned".format(update.message.reply_to_message.from_user.id, update.message.reply_to_message.from_user.first_name, update.message.reply_to_message.from_user.username))
     except:
