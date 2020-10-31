d73edb64bf6eb45472c253080083f4596833a415
mik3sw <mikedev.xda@gmail.com>
2020-10-31 12:16:44 +0100

    ⚙️  [Fixed] non-latin filter logic

--- bot/dialogs/images.py
+++ bot/dialogs/images.py
@@ -11,7 +11,7 @@ def init(update, context):
         capt = update.message.caption
         china = bool(re.findall(r'[\u4e00-\u9fff]+', capt))
         arabic = bool(re.findall(r'[\u0600-\u06ff]|[\u0750-\u077f]|[\ufb50-\ufbc1]|[\ufbd3-\ufd3f]|[\ufd50-\ufd8f]|[\ufd92-\ufdc7]|[\ufe70-\ufefc]|[\uFDF0-\uFDFD]+',capt))
-        russia = bool(re.search('[а-яА-Я]', update.message.from_user.first_name))
+        russia = bool(re.search('[а-яА-Я]', capt))
         spam = custom_spam(str(capt).lower())
         
         if china or arabic or russia or spam:
--- bot/dialogs/rules/non_latin_filter.py
+++ bot/dialogs/rules/non_latin_filter.py
@@ -4,9 +4,9 @@ from configparser import ConfigParser
 def arabic(update, context, r1):
     if r1.get('chat', 'arabic_characters') == 'True':
         #china = re.findall(r'[\u4e00-\u9fff]+', new.first_name)
-        arabic = re.findall(r'[\u0600-\u06ff]|[\u0750-\u077f]|[\ufb50-\ufbc1]|[\ufbd3-\ufd3f]|[\ufd50-\ufd8f]|[\ufd92-\ufdc7]|[\ufe70-\ufefc]|[\uFDF0-\uFDFD]+',update.message.from_user.first_name)
-        if arabic != []:
-            context.bot.kick_chat_member(update.message.chat.id, update.message.from_user.id, timeout=None, until_date=None)
+        arabic = bool(re.findall(r'[\u0600-\u06ff]|[\u0750-\u077f]|[\ufb50-\ufbc1]|[\ufbd3-\ufd3f]|[\ufd50-\ufd8f]|[\ufd92-\ufdc7]|[\ufe70-\ufefc]|[\uFDF0-\uFDFD]+',update.message.from_user.first_name))
+        arabic1 = bool(re.findall(r'[\u0600-\u06ff]|[\u0750-\u077f]|[\ufb50-\ufbc1]|[\ufbd3-\ufd3f]|[\ufd50-\ufd8f]|[\ufd92-\ufdc7]|[\ufe70-\ufefc]|[\uFDF0-\uFDFD]+',update.message.text))
+        if arabic or arabic1:
             update.message.reply_text("User [{}][{}][@{}] Banned\n<b>Reason:</b> non_latin_filter triggered [arabic characters]".format(update.message.from_user.id, update.message.from_user.first_name, update.message.from_user.username), parse_mode = 'HTML')
             return True
         else:
@@ -17,10 +17,9 @@ def arabic(update, context, r1):
 
 def chinese(update, context, r1):
     if r1.get('chat', 'chinese_characters') == 'True':
-        china = re.findall(r'[\u4e00-\u9fff]+', update.message.from_user.first_name)
-        #arabic = re.findall(r'[\u0600-\u06ff]|[\u0750-\u077f]|[\ufb50-\ufbc1]|[\ufbd3-\ufd3f]|[\ufd50-\ufd8f]|[\ufd92-\ufdc7]|[\ufe70-\ufefc]|[\uFDF0-\uFDFD]+',new.first_name)
-        if china != []:
-            context.bot.kick_chat_member(update.message.chat.id, update.message.from_user.id, timeout=None, until_date=None)
+        china = bool(re.findall(r'[\u4e00-\u9fff]+', update.message.from_user.first_name))
+        china1 = bool(re.findall(r'[\u4e00-\u9fff]+', update.message.text))
+        if china or china1:
             update.message.reply_text("User [{}][{}][@{}] Banned\n<b>Reason:</b> non_latin_filter triggered [chinese characters]".format(update.message.from_user.id, update.message.from_user.first_name, update.message.from_user.username), parse_mode = 'HTML')
             return True
         else:
@@ -31,8 +30,8 @@ def chinese(update, context, r1):
 def russian(update, context, r1):
     if r1.get('new_user', 'russian_characters') == 'True':
         russia = bool(re.search('[а-яА-Я]', update.message.from_user.first_name))
-        if russia:
-            context.bot.kick_chat_member(update.message.chat.id, update.message.from_user.id, timeout=None, until_date=None)
+        russia1 = bool(re.search('[а-яА-Я]', update.message.text))
+        if russia or russia1:
             update.message.reply_text("User [{}][{}][@{}] Banned\n<b>Reason:</b> non_latin_filter triggered [russian characters]".format(update.message.from_user.id, update.message.from_user.first_name, update.message.from_user.username), parse_mode = 'HTML')
             return True
         else:
@@ -45,6 +44,9 @@ def init(update, context):
     if update.message is not None:
         r1 = ConfigParser()
         r1.read('settings.ini')
+        
         if chinese(update, context, r1) or arabic(update, context, r1) or russian(update, context, r1):
-            print("latin filter triggered") 
+            print("latin filter triggered")
+            context.bot.delete_message(update.message.chat_id, update.message.message_id)
+            context.bot.kick_chat_member(update.message.chat.id, update.message.from_user.id, timeout=None, until_date=None) 
     
