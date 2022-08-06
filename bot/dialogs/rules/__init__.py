b441d126cfeba5aa400dca332c7ec09d2d8c4a9c
Luca Giaccone <luca.giaccone@polito.it>
2022-08-06 16:47:19 +0200

    Use tag '@admin' to nofity admins in their private grourp.
     Changes to be committed:
        modified:   config.py
        modified:   dialogs/rules/__init__.py
        new file:   dialogs/rules/admin_tag.py
        modified:   dialogs/rules/main.py

--- bot/dialogs/rules/__init__.py
+++ bot/dialogs/rules/__init__.py
@@ -1,3 +1,3 @@
-__all__ = ["main", "urlfilter", "bad_words", "non_latin_filter", "market"]
+__all__ = ["main", "urlfilter", "bad_words", "non_latin_filter", "market", "admin_tag"]
 
 from dialogs.rules import *
