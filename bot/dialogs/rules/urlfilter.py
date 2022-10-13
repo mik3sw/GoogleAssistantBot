cca68239f8145c8d88b23011364e105718a5e470
mik3sw <mikedev.xda@gmail.com>
2022-10-13 11:05:06 +0200

    urlfilter fix

--- bot/dialogs/rules/urlfilter.py
+++ bot/dialogs/rules/urlfilter.py
@@ -6,7 +6,17 @@ def init(update, context):
     if update.message is None:
         return
     if not(update.message.from_user.id in config.LIST_OF_ADMINS):
-        for x in config.url_denylist and x not in config.url_whitelist:
+        for x in config.url_denylist:
             if x in update.message.text:
-                context.bot.delete_message(update.message.chat_id, update.message.message_id)
-                context.bot.send_message(update.message.chat_id, text="This link has been blocked by an administrator")
+                spam = True
+                for link in config.url_whitelist:
+                    if link in update.message.text.lower():
+                        spam = False
+            else:
+                spam = False
+
+        if spam:
+            context.bot.send_message(config.admin_group, text="(DEBUG MESSAGE)\nIl seguente messaggio e' stato bloccato:\n\n{}".format(update.message.text))
+                    
+            context.bot.delete_message(update.message.chat_id, update.message.message_id)
+            context.bot.send_message(update.message.chat_id, text="This link has been blocked by an administrator")
