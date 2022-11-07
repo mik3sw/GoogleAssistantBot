b68d43d1b100420eb91f36f1a7f022bb909c95f6
mik3sw <mikedev.xda@gmail.com>
2022-11-07 20:20:59 +0100

    config file

--- bot/config.py
+++ bot/config.py
@@ -1,37 +1,34 @@
 # Bot configuration file
 # ======================
-bot_token = '621843590:AAGURba__YpuximGPR0a5_x3-DwFph14-Io'
-bot_username = 'pythonandroidbot'
-bot_id = '621843590'
-log_channel = -1001323415459 #38201859 me
-language = 'it' #check 'strings.ini' file
-OWNER_LIST= [38201859,605363037]
-LIST_OF_ADMINS = [38201859, 77420618, 147276542, 400836959, 880456922]
-COLLABORATORS = [69991922]
+bot_token = "000000000:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+bot_username = "mybotusername"
+bot_id = 00000000000
+log_channel = -1010101010101010
+language = 'it' #check 'strings.ini' file [en, es, ru, ...]
+OWNER_LIST= [00000000]
+LIST_OF_ADMINS = [11111111, 2222222, 3333333]
+COLLABORATORS = [44444444]
 TITLES = ['creator', 'administrator'] #do not change
 # =======================
 
 # Plugins
 # =======================
-covid_19_report = [-1001264240585, 38201859]
-weather_report = [-1001264240585, 38201859]
-weather_api = '772340c8970fe60a021d5608462b2ec9'
+weather_report = [-00000000000, -111111111]
+weather_api = 'abcdefghi0123456789'
 # =======================
 
 # Chat controls
 # =======================
-bad_words = ['porco dio', 'porca madonna'] #messages with this words will be deleted
+bad_words = ['word1', 'word2'] #messages with this words will be deleted
 # =======================
-
 
 # URL Filter
 # =======================
-#url_denylist = ["amzn.to", "t.me"]
-url_denylist = ["t.me"]
+url_denylist = ["amzn.to", "t.me"]
 url_whitelist = ["t.me/googlepixelit", "t.me/aospitalia", "t.me/aospitaliashop", "t.me/pixelwatchitalia", "t.me/googlepixelitchannel"]
 
 # Market id
-mercatino = -1001160935294
+mercatino = -111111111
 
-
-admin_group = -1001323415459
+# admin group is
+admin_group = -1111111111111
