41472b60c468ecfd16e7ad5c1651c878b2daf531
mik3sw <mikedev.xda@gmail.com>
2022-11-09 17:36:55 +0100

    test

--- bot/config.py
+++ bot/config.py
@@ -1,30 +1,37 @@
 # Bot configuration file
 # ======================
-bot_token = 'TOKEN'
-bot_username = 'username'
-bot_id = '0000000'
-log_channel = -123456789
+bot_token = '621843590:AAGURba__YpuximGPR0a5_x3-DwFph14-Io'
+bot_username = 'pythonandroidbot'
+bot_id = '621843590'
+log_channel = -1001323415459 #38201859 me
 language = 'it' #check 'strings.ini' file
-OWNER_LIST= [123456,3456789]
-LIST_OF_ADMINS = [12345, 111111, 22222]
-COLLABORATORS = [12345667]
+OWNER_LIST= [38201859,605363037]
+LIST_OF_ADMINS = [38201859, 77420618, 147276542, 400836959, 880456922]
+COLLABORATORS = [69991922]
 TITLES = ['creator', 'administrator'] #do not change
 # =======================
 
+# Plugins
+# =======================
+covid_19_report = [-1001264240585, 38201859]
+weather_report = [-1001264240585, 38201859]
+weather_api = '772340c8970fe60a021d5608462b2ec9'
+# =======================
 
 # Chat controls
 # =======================
-bad_words = ['word1', 'word2'] # messages with this words will be deleted
+bad_words = ['porco dio', 'porca madonna'] #messages with this words will be deleted
 # =======================
 
 
 # URL Filter
 # =======================
+#url_denylist = ["amzn.to", "t.me"]
 url_denylist = ["t.me"]
 url_whitelist = ["t.me/googlepixelit", "t.me/aospitalia", "t.me/aospitaliashop", "t.me/pixelwatchitalia", "t.me/googlepixelitchannel"]
 
 # Market id
-mercatino = -12345678
+mercatino = -1001160935294
 
 
-admin_group = -1234567
+admin_group = -1001323415459
