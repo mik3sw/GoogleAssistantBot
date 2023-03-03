41af4e173faa8dbb7159e6b66580f5d2087415f8
Luca Giaccone <luca.giaccone@polito.it>
2023-03-03 21:28:27 +0100

    Major update to PTB v20 (commit 7/7)
    Changes to be committed:
    	modified:   utils/decorator.py

--- bot/utils/decorator.py
+++ bot/utils/decorator.py
@@ -1,43 +1,45 @@
 # DEFINIZIONE COMANDI ADMIN
 from functools import wraps
 import config
-from time import sleep
 from rich.console import Console
 
+# initialize console
 console = Console()
+
 
 # only people from config.LIST_OF_ADMINS can perform that command
 def restricted(func):
     @wraps(func)
-    def wrapped(update, context):
+    async def wrapped(update, context):
         user_id = update.effective_user.id
         if user_id not in config.LIST_OF_ADMINS:
             console.log("Unauthorized access denied for {}.".format(user_id))
             return
-        return func(update, context)
+        return await func(update, context)
     return wrapped
+
 
 # delete command message (ex: /ban)
 def cancellacomandi(func):
     @wraps(func)
-    def wrapped(update, context):
-        bot = context.bot
+    async def wrapped(update, context):
         if update.message.text is not None:
-          if update.message.text.startswith("/"):
-            bot.delete_message(update.message.chat_id, update.message.message_id)
-        return func(update, context)
+            if update.message.text.startswith("/"):
+                await context.bot.delete_message(update.message.chat_id, update.message.message_id)
+        return await func(update, context)
     return wrapped
+
 
 # Owner of the bot
 # only people from config.OWNER_LIST can perform that command
 def ownerbot(func):
     @wraps(func)
-    def wrapped(update, context):
+    async def wrapped(update, context):
         user_id = update.effective_user.id
         if user_id not in config.OWNER_LIST:
-            cnsole.log("Unauthorized access denied for {}.".format(user_id))
+            console.log("Unauthorized access denied for {}.".format(user_id))
             return
-        return func(update, context)
+        return await func(update, context)
     return wrapped
 
 
@@ -45,16 +47,19 @@ def ownerbot(func):
 # only group's admin can perform that command/press that InlineButton
 def general_admin(func):
     @wraps(func)
-    def wrapped(update, context):
+    async def wrapped(update, context):
         user_id = update.effective_user
         try:
-            stat = context.bot.get_chat_member(update.message.chat_id, update.effective_user['id'])['status']
+            stat = await context.bot.get_chat_member(update.message.chat_id, update.effective_user['id'])
+            stat = stat['status']
         except:
-            stat = context.bot.get_chat_member(update.callback_query.message.chat_id, update.callback_query.from_user['id'])['status']
+            stat = await context.bot.get_chat_member(update.callback_query.message.chat_id, update.callback_query.from_user['id'])
+            stat = stat['status']
+
         if stat in config.TITLES:
             console.log("Action performed by: {}".format(stat))
         else:
             console.log("Unauthorized access denied for {}.".format(user_id['id']))
             return
-        return func(update, context)
+        return await func(update, context)
     return wrapped
